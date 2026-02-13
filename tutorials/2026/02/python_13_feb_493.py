bb1236fba4aca68ef7cb4a734b456b095cb6ce79
HybridCore7 <142479108+HybridCore7@users.noreply.github.com>
2026-02-13 17:25:48 +0000

    Add python tutorial [Feb 13, 2026]

---
+++ tutorials/2026/02/python_13_feb_493.py
@@ -0,0 +1,8 @@
+# Trie Data Structure in Python
+=====================================
+
+## Introduction
+A trie, also known as a prefix tree, is a data structure that stores a collection of strings. It is commonly used in autocomplete and spell-check applications.
+
+## Implementing Trie in Python
+--------------------------------
