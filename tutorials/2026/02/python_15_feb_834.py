da9727b513e49d9982160657752b761b3ba282c7
HybridCore7 <142479108+HybridCore7@users.noreply.github.com>
2026-02-15 16:54:54 +0000

    Add python tutorial [Feb 15, 2026]

---
+++ tutorials/2026/02/python_15_feb_834.py
@@ -0,0 +1,4 @@
+# Two Pointers Technique
+=====================================
+
+The two pointers technique is a common approach used in array and linked list problems. It's a simple yet effective way to solve a variety of problems.
