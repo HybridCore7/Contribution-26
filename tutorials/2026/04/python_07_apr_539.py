c5cdbf4ffdb94b42d19f80b09a297969f0ea237d
HybridCore7 <142479108+HybridCore7@users.noreply.github.com>
2026-04-07 10:58:40 +0000

    Add python tutorial [Apr 07, 2026]

---
+++ tutorials/2026/04/python_07_apr_539.py
@@ -0,0 +1,4 @@
+# Segment Tree Implementation in Python
+=====================================================
+
+This is an implementation of the segment tree data structure in Python. The segment tree allows for efficient range queries on arrays.
